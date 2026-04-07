e34f6ea2ad0ddb482a6d66709f55456d5f768ea4
ashwinmali7781 <ashwinmali72@gmail.com>
2026-04-07 19:09:08 +0530

    hospital

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -24,7 +24,7 @@ const orderSchema = new mongoose.Schema(
     orderItems: {
       type: [orderItemSchema],
       type: [],
-    },
+    },   
     address:{
         type: String,
         required: true
