7d60e3ae0163a6b40c6238d1f6815c6c698b1987
ashwinmali7781 <ashwinmali72@gmail.com>
2026-04-07 19:22:03 +0530

    const

--- models/hospital-management/patient.models.js
+++ models/hospital-management/patient.models.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const medicalSchema = new mongoose.Schema({}, { timestamps: true });
+const patientSchema = new mongoose.Schema({}, { timestamps: true });
 
 export const Patient = mongoose.model("Medical",medicalSchema);
 
